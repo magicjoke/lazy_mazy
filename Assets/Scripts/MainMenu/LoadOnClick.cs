a3ca1e7680d1c93e735b057e5f341b164df6e8c7
Askariot <magicjoke@gmail.com>
2018-09-29 00:29:56 +0300

    Revert "Reverting"
    This reverts commit c6f1f020268a0abd43322fac4d53d82172e7cac1.

---
+++ Assets/Scripts/MainMenu/LoadOnClick.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadOnClick : MonoBehaviour {
+
+    public string levelToLoad;
+
+    private GameObject levelSelectController;
+    private GameObject levelFader;
+
+    private float waitTime = 1f;
+    private float smallWaitTime = 2f;
+
+    private void Awake()
+    {
+        levelSelectController = GameObject.FindGameObjectWithTag("LevelSelectController");
+        levelFader = GameObject.FindGameObjectWithTag("LevelFader");
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void OnMouseDown()
+    {
+        StartCoroutine(LoadLevel());
+        //levelSelectController.GetComponent<LevelSelectController>.two = true;
+    }
+
+    //if(levelSelectController.GetComponent<LevelSelectController>.)
+    IEnumerator LoadLevel()
+    {
+        levelSelectController.GetComponent<LevelSelectController>().two = true;
+        yield return new WaitForSeconds(smallWaitTime);
+        levelFader.GetComponent<Animator>().Play("Level_fade_1");
+        yield return new WaitForSeconds(waitTime);
+        SceneManager.LoadScene(levelToLoad);
+
+    }
+}
